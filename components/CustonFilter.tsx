ad24cc0c59d719b9ac6e5bc0cecdf75e424be200
Abdurrazaq Awwal <109519925+PomaCray@users.noreply.github.com>
2023-11-06 23:33:02 +0100

    commit suicide

---
+++ components/CustonFilter.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CustonFilter() {
+  return (
+    <div>CustonFilter</div>
+  )
+}
---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import Hero from './Hero'
+import CustomButton from './CustomButton'
+import Footer from './Footer'
+import Navbar from './Navbar'
+import CustonFilter from './CustonFilter'
+import SearchBar from './SearchBar'
+import SearchManufacturer from './SearchManufacturer'
+import CarCard from './CarCard'
+import CarDetails from './CarDetails'
+
+export{
+    Hero,
+    CustomButton,
+    Footer,
+    Navbar,
+    CustonFilter,
+    SearchBar,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+
+}
